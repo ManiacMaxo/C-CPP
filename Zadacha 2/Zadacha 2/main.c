db5c747855eefee3fe4da451a8d7567f80477257
Victor Gorchilov <victor@gorchilov.com>
2018-11-08 16:00:19 +0200

    update

--- Zadacha 2/Zadacha 2/main.c
+++ Zadacha 2/Zadacha 2/main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 void concatenate(char *str1, char *str2, char *str3) {
-    int i, k;
-    for (i=0; i<32; i++)
+    int i, k, len1=strlen(str1), len2=strlen(str2);
+    for (i=0; i<len1; i++)
         str3[i] = str1[i];
-    for (k=0; k<64; k++, i++)
+    i = len1;
+    for (k=0; k<len1+len2-1; k++, i++)
         str3[i] = str2[k];
-    str3[64] = '\0';
+    str3[len1+len2-1] = '\0';
 }
 
 int main() {
-    char str1[32], str2[32], str3[64];
+    char str1[100], str2[100], str3[200];
     printf("string 1: ");
-    fgets(str1, 32, stdin);
+    scanf("%s", &str1);
     printf("string 2: ");
-    fgets(str2, 32, stdin);
+    scanf("%s", &str2);
     concatenate(str1, str2, str3);
-    printf("concatenated string: %s", str3);
+    printf("concatenated string: %s\n", str3);
 }
---
+++ zad 6/zad 6/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int toInteger(char *str) {
+    int i, numStr=0, k=strlen(str);
+    for (i=0; i<strlen(str); i++, k--) {
+        numStr += (str[i] - '0') * pow(i, k);
+    }
+    //    numStr = (str[1] - '0') * 100 + (str[2] - '0') * 10 + (str[3] - '0');
+    return numStr;
+}
+
+int main(int argc, char **argv) {
+    int i, sum = 0;
+    for (i=0; i<argc; i++) {
+        sum += toInteger(argv[i]);
+    }
+    printf("%d", sum);
+}
