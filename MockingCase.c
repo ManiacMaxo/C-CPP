8c5dd1d1659e6954185c57e55be971b04e348572
Victor Gorchilov <victor@gorchilov.com>
2018-11-15 17:40:47 +0200

    update

--- MockingCase.c
+++ MockingCase.c
@@ -4,7 +4,7 @@
 
 void toMockingCase(char *str) {
     int i, len=strlen(str);
-    for (i=0; i<len; i++) {
+    for(i=0; i<len; i++) {
         if(!((str[i] > 'a' && str[i] < 'z') || (str[i] > 'A' && str[i] < 'Z')))
             continue;
         else if(i % 2 == 0) {
@@ -15,8 +15,7 @@ void toMockingCase(char *str) {
 }
 
 int main(int argc, char **argv) {
-    int i;
-    for (i=1; i<argc; i++) {
+    for(int i=1; i<argc; i++) {
         toMockingCase(argv[i]);
         printf("%s", *argv);
     }
