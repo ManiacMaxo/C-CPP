8e45cca4f56ba6f286d2c9d3e5714cee2b594a42
Victor Gorchilov <victor@gorchilov.com>
2018-12-02 23:19:26 +0200

    Create sum_int_arr.c

---
+++ sum_int_arr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int aVeryBigSum(int *arr, int n) {
+    int i;
+    long int sum = 0;
+    for (i = 0; i < n; i++)
+        sum += arr[i];
+    return sum;
+}
+
+int main() {
+    int arr[10], n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    printf("%d", aVeryBigSum(arr, n));
+}
