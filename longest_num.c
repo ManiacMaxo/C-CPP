d721858af27e4f9f6e976e2c57e3dff1834e1233
Victor Gorchilov <victorgorchilov@Victors-MacBook-Air.local>
2019-03-28 10:02:08 +0200

    exam task 1

---
+++ longest_num.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int longest_num(char *str) {
+    int longest = 0, count = 0;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            if (!(count == 0 && str[i] == '0')) {
+                count++;
+                if (count > longest) {
+                    longest = count;
+                }
+            }
+        } else {
+            count = 0;
+        }
+    }
+    return longest;
+}
+
+int main() {
+    char str[250];
+    fgets(str, 250, stdin);
+    printf("%d\n", longest_num(str));
+}
