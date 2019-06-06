1fa9583a5285b644389623555d56f4d8f9ef7a6c
Victor Gorchilov <victorgorchilov@Victors-MacBook-Air.local>
2019-06-06 21:33:10 +0300

    hw

--- Dijkstra.c
+++ Dijkstra.c
@@ -2,33 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int **map;
-/* 
-int find(int start, int finish, int from, int n) {
-    if (dist[start] > 0) {
-        return dist[start];
-    }
-    int min = INT_MAX, res;
-    dist[start] = min;
-    for (int i = 0; i < n; i++) {
-        if (i == from) {  // skip the branch we came from
-            continue;
-        }
-        if (map[start][i] > 0) {  // is there a direct connection?
-            if (i == finish) {    // is that the final node we're looking for?
-                res = map[start][i];
-            } else {  // recursive dive
-                res = map[start][i] + find(i, finish, start, n);
-            }
-        }
-        if (res < min) min = res;
-        //        if (min == 1) break;  // don't go further if distance of 1 is found.
-    }
-    dist[start] = min;
-    return min;
-}
- */
-
 void print(int *dist, int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", dist[i]);
@@ -36,65 +9,102 @@ void print(int *dist, int n) {
     printf("\n");
 }
 
-int find(int start, int finish, int n) {
-    int *dist = malloc(n * sizeof(int));
-    int *been = malloc(sizeof(dist));
-    int i, j, next;
+int dijkstra(int start, int finish, int n, int map[n][n]) {
+    int dist[n], been[n];
+    int next;
+    int i, j;
 
-    for (j = 0; j < n; j++) {  // first point
-        been[j] = 0;
-        dist[j] = map[start][j];
-        if (map[start][j] == 0) {
-            dist[j] = INT_MAX;
-        }
+    for (i = 0; i < n; i++) {  // distance from first point
+        been[i] = 0;
+        dist[i] = map[start][i];
     }
     been[start] = 1;
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {  // find next point
         if (dist[i] != -1 && dist[i] != INT_MAX) {
             next = i;
             break;
         }
     }
 
-    for (i = 0; i < n; i++) {
+    for (int k = 1; k < n - 1; k++) {
         print(dist, n);
         for (j = 0; j < n; j++) {
-            if (map[i][j] > 0 && !been[j] && j != finish) {
-                if ((dist[next] + map[i][j]) < dist[j]) {
-                    dist[j] = map[i][j];
+            if (dist[next] + map[next][j] < dist[j] && been[j] == 0 && next != j) {
+                dist[j] = dist[next] + map[next][j];
+            }
+        }
+
+        been[next] = 1;
+        for (i = 0; i < n; i++) {
+            if (dist[i] != INT_MAX && been[i] == 0 && i != finish) {
+                next = i;
+                break;
+            }
+        }
+    }
+    return dist[finish];
+}
+/* 
+int find(int start, int finish, int n, int map[n][n]) {
+    int dist[n];  // from start to point i
+    int *been = malloc(n * sizeof(int));
+    i, j, next;
+
+    for (i = 0; i < n; i++) {  // from first point
+        dist[i] = map[start][i];
+        been[i] = 0;
+    }
+    been[start] = 1;
+    print(dist, n);
+
+    for (i = 0; i < n; i++) {  // next from first
+        if (dist[i] != -1 && dist[i] != INT_MAX) {
+            next = i;
+            break;
+        }
+    }
+
+    for (i = 0; i < n - 1; i++) {  // every other point except the finish
+        print(dist, n);
+        printf("next: %d\n", next);
+        for (j = 0; j < n; j++) {
+            if (!been[j] && j != finish) {
+                if (dist[next] + map[i][j] < dist[j]) {
+                    dist[j] = map[i][j] + dist[next];
                 }
             }
-            been[j] = 1;
-            for (i = 0; i < n; i++) {
-                if (dist[i] != INT_MAX && !been[i] && i != finish) {
-                    next = i;
-                    break;
-                }
+        }
+        been[next] = 1;
+        for (int k = 0; k < n; k++) {
+            if (dist[k] != INT_MAX && !been[k] && k != finish) {
+                next = k;
+                break;
             }
         }
     }
     free(been);
-    free(dist);
     return dist[finish];
 }
-
+ */
 int main() {
     FILE *file = fopen("in.txt", "r");
     int n;
     fscanf(file, "%d", &n);
-    map = malloc(n * sizeof(int));
+    int map[n][n];
 
     for (int i = 0; i < n; i++) {  // set adj matrix
-        map[i] = malloc(n * sizeof(int));
         for (int j = 0; j < n; j++) {
             fscanf(file, "%d", &map[i][j]);
+            if (map[i][j] == 0) {
+                map[i][j] = INT_MAX;
+            }
         }
     }
     fclose(file);
     int start, finish;
 
+    printf("Enter start and finish:");
     scanf("%d %d", &start, &finish);
-    printf("%d\n", find(start, finish, n));
-    free(map);
+    printf("%d\n", dijkstra(start, finish, n, map));
 }
