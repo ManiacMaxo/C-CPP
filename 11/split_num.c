d906498e2f27f359c65a955cc2c960a7aa8f742f
Victor Gorchilov <unconfigured@null.spigotmc.org>
2019-09-19 12:15:41 +0300

    first exercise of the year

---
+++ 11/split_num.c
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void split_num(int num, int len) {
+    int i = 0;
+    int arr[len];
+    while (num) {
+        arr[i] = num % 10;
+        num /= 10;
+        i++;
+    }
+
+    int j = len - 1;
+    for (i = 0; i <= j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    for (i = 0; i < len; i++) {
+        printf("%d", arr[i]);
+        if (i != len - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+    int num = atoi(argv[1]);
+    int len = floor(log10(abs(num))) + 1;  // find the length of the number
+    split_num(num, len);
+}
