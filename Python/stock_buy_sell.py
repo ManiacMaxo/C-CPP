c45aed014c4fd6cdbd221093c6c4a7fdd098eeb8
Victor <victor@gorchilov.com>
2019-09-30 18:59:43 +0300

    TP

--- Python/stock_buy_sell.py
+++ Python/stock_buy_sell.py
@@ -1,51 +1,37 @@
 stocks = [100, 180, 260, 310, 40, 535, 695]
 
 
-def find_max(start):
-    a_max = 0
-    pos = None
-    a_len = len(stocks)
-    for i in range(start, a_len):
-        if(stocks[i] > a_max):
-            a_max = stocks[i]
-            pos = i
-    return pos
-
-
-def find_min(end):
+def find_min_max(start, end):
     a_min = float('inf')
-    pos = None
-    a_len = len(stocks)
-    for i in range(a_len):
+    a_max = 0
+    pos = []
+    for i in range(start, end):
         if (stocks[i] < a_min):
             a_min = stocks[i]
-            pos = i
+            pos[0] = i
+        if (stocks[i] > a_max):
+            a_max = stocks[i]
+            pos[1] = i
     return pos
 
 
 def main():
-    if (stocks == stocks.sort(reverse=True)):
+    if (stocks == stocks.sort(reverse = True)):
         print('Stocks are decreasing')
         exit()
-    s_min = float('inf')
-    s_max = 0
-    d_min = d_max = None
-    s_len = len(stocks)
-    d_max = find_max(0)
-    d_min = find_min(0)
-    j = s_len
-    for i in range(s_len):
-        if (d_max == i):
-            d_max = find_max(i+1)
-        if (d_min == j):
-            d_min = find_min(j-1)
-        j -= 1
-    print(d_min, d_max)
-    s_max = stocks[d_max]
-    s_min = stocks[d_min]
-    if (d_min < d_max):
-        print('Maximum profit: ', s_max - s_min,
-              '\t When you buy on day %d and sell on day %d', d_min, d_max)
+    a_len = len(stocks)
+    day = find_min_max(0, a_len)
+    print(day[0], day[1])
+    if (day[0] > day[1]):
+        from_maxes = find_min_max(0, day[1]) # smallest number b4 max
+        from_mins = find_min_max(day[0], a_len) # largest number after min
+        if(stocks[from_maxes[1]] - stocks[from_maxes[0]] > stocks[from_mins[1]] - stocks[from_mins[0]]):
+            day[0] = from_maxes[0]
+            day[1] = from_maxes[1]
+        else:
+            day[0] = from_mins[0]
+            day[1] = from_mins[1]
+    print('Maximum profit: %d\t When you buy on day %d and sell on day %d' % (stocks[day[1]] - stocks[day[0]], day[0], day[1]))
 
 
 main()
