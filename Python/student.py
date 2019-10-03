6b6d991b66c5273a4b2f276097519e126b8c5ae8
Victor <victor@gorchilov.com>
2019-10-03 08:54:12 +0300

    Added error check

--- Python/student.py
+++ Python/student.py
@@ -1,20 +1,15 @@
+#!python3
 import statistics
-import readchar  # ----------------please pip install----------------
+import readchar  # ----------------this needs to be installed----------------
 
 
 class Student:
-    def __init__(self, name, num_class):
+    def __init__(self, name, num):
         self.name = name
-        self.num_class = num_class
+        self.num = num
         self.grades = dict()
 
     def add_grade(self, val, subject):
-        if not subject in self.grades:
-            print('Such a subject does not exist!\nIf you want to add it, press 1')
-            if (readchar.readchar() == '1'):
-                self.add_subject(subject)
-            return
-            # self.grades[subject] = []
         self.grades[subject].append(val)
 
     def avg_grade(self, subject):
@@ -27,18 +22,81 @@ class Student:
             return
         self.grades[subject] = []
 
+    def print_grades(self, subject):
+        print('%s: %s' % (subject, self.grades[subject]))
+
+
+def input_subject(student):
+    subject = input('Enter subject: ').strip().title()
+    while(subject not in student.grades):
+        subject = input(
+            'This subject does not exists! Enter a valid subject: ').strip().title()
+    return subject
+
+
+def input_grade(student):
+    grade = input('Enter a grade: ')
+    while(not grade.isnumeric() or (int(grade) < 2 or int(grade) > 6)):  # error check
+        grade = input('Please enter a valid grade: ')
+    return int(grade)
+
 
 def main():
-    name = input('Name of student: ')
-    num_class = input('Number in class: ')
-    student = Student(name, num_class)
+    name = input('Name of student: ').title()
+    num = input('Number of student: ')
+
+    while(not num.isnumeric()):  # error check
+        num = input('Please enter a number')
+    num = int(num)
+
+    student = Student(name, num)
+
     subjects = ['Math', 'English', 'Geography',
                 'History', 'Chemistry', 'Biology', 'Physics']  # base subjects
     for subject in subjects:
         student.add_subject(subject)
-    student.add_subject('Russian')
-    student.add_grade(4, 'Russian')
-    print(student.grades)
+
+    while(42):
+        print('[Q]uit | Add [G]rade | Add [S]ubject | Get [A]verage For Subject | [P]rint grades: ',
+              end='', flush=True)  # no newline
+        operation = readchar.readchar().lower()
+        print(operation)
+
+        if operation == 'q':  # quit
+            exit()
+
+        elif operation == 'g':  # add grade
+            subject = input_subject(student)
+            grade = input_grade(student)
+
+            student.add_grade(grade, subject)
+
+        elif operation == 's':  # add subject
+            subject = input_subject(student)
+            student.add_subject(subject)
+
+        elif operation == 'a':  # average grade
+            subject = input_subject(student)
+            student.avg_grade(subject)
+
+        elif operation == 'p':  # print grades
+            print('[A]ll or for One [S]ubject: ', end='', flush=True)
+            op = readchar.readchar().lower()
+            print(op)
+
+            if op == 'a':  # print all subjects
+                for subject in student.grades:
+                    student.print_grades(subject)
+
+            elif op == 's':  # print only one given subject
+                subject = input_subject(student)
+                student.print_grades(subject)
+
+            else:
+                print('Unknown operation. Try again')
+
+        else:
+            print('Unknown operation. Try again')
 
 
 main()
