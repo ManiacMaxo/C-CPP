a6053f6be16e5169741eb5254f116a6e1d0f664d
Victor <victor@gorchilov.com>
2019-10-06 20:43:01 +0300

    OS hw

--- C/void_swap.c
+++ C/void_swap.c
@@ -2,27 +2,31 @@
 // NAME: Victor Gorchilov
 // CLASS: 11a
 // NUMBER: 8
-// PROBLEM: #3
-// FILE NAME: swap.c (unix file name)
-// FILE PURPOSE: swaping two integers
+// PROBLEM: #4
+// FILE NAME: void_swap.c (unix file name)
+// FILE PURPOSE: swaping two variables
 // ------------------------------------------------------------------------
-
 #include <stdio.h>
 
-void swap(int *a, int *b) {
+void swap(void *a, void *b, size_t len) {
     //------------------------------------------------------------------------
     // FUNCTION: swap
-    // swap two integers
+    // swap two pointers
     // PARAMETERS:
-    // pointers to two integers
+    // two pointers to swap
     //------------------------------------------------------------------------
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+    unsigned char *p = a, *q = b, tmp;
+    for (size_t i = 0; i != len; ++i) {
+        tmp = p[i];
+        p[i] = q[i];
+        q[i] = tmp;
+    }
 }
 
 int main() {
-    int a = 1, b = 2;
-    swap(&a, &b);
-    printf("%d  %d", a, b);
+    int a, b;
+    a = 5;
+    b = 7;
+    swap(&a, &b, 10);
+    printf("%d %d\n", a, b);
 }
