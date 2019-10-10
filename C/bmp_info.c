d127236a9020ecd93633457f48fb698909ded17f
Victor <victor@gorchilov.com>
2019-10-10 11:50:12 +0300

    OS hw

--- C/bmp_info.c
+++ C/bmp_info.c
@@ -1,7 +1,11 @@
+#include <stdint.h>
 #include <stdio.h>
 
 void *read_image(const char *filepath);
 
-int main(int argc, char argv[]) {
-    char fname[] = "image.bmp";
+int main(int argc, char **argv) {
+    char *ptr = read_image(argv[1]);
+    uint32_t w = *(uint32_t *)&ptr[18];
+    uint32_t h = *(uint32_t *)&ptr[22];
+    printf("BMP image width: %d\nBMP image height: %d\n", w, h);
 }
