cee0750b101707fe3e11024df6644f385adf1f08
Victor <victor@gorchilov.com>
2019-10-14 08:02:13 +0300

    OS hw

--- C/png.c
+++ C/png.c
@@ -6,22 +6,23 @@
 void *read_image(const char *filepath);
 
 int main(int argc, char **argv) {
-    char *result = strstr(argv[1], ".png");
-    int pos = result - argv[1];  // get position of ststsr if exists
-    if (pos != strlen(argv[1]) - 4) {
-        printf("Unable to parse file\n");
-        return 0;
-    }
-    if (read_image(argv[1]) == NULL) {
+    if (argv[1] == NULL || read_image(argv[1]) == NULL) {  // does the file exist
         printf("Unable to open file\n");
         return 0;
     }
+    char *find = strstr(argv[1], ".png");
+    int pos = find - argv[1];          // get position of ststsr if exists
+    if (pos != strlen(argv[1]) - 4) {  // is the file a png
+        printf("Unable to parse file\n");
+        return 0;
+    }
+
     struct stat st;
-    stat(argv[1], &st);
+    stat(argv[1], &st);  // size of file in bits
 
     char *ptr = read_image(argv[1]);
-    uint32_t w = *(uint32_t *)&ptr[18];
-    uint32_t h = *(uint32_t *)&ptr[22];
+    int w = ntohl(*(uint32_t *)&ptr[16]);
+    int h = ntohl(*(uint32_t *)&ptr[20]);
     printf("PNG image width: %d\nPNG image height: %d\n", w, h);
     printf("PNG file size: %lld\n", st.st_size);
 }
