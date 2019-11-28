4f2a8ce0365bfd89b98b1c7f5a55286e7f4a0ea8
Victor <victor@gorchilov.com>
2019-11-28 17:29:40 +0200

    tail

--- C/tail/tail.c
+++ C/tail/tail.c
@@ -1,3 +1,12 @@
+//------------------------------------------------------------------------
+// NAME: Victor Gorchilov
+// CLASS: 11a
+// NUMBER: 8
+// FILE NAME: tail.c (unix file name)
+// FILE PURPOSE:
+// implementation of tail function in c
+//------------------------------------------------------------------------
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,32 +16,29 @@
 #define BUFSIZE 10000
 
 int find_start(int fd) {
-    printf("entered find start\n");
-    printf("%lld\n", lseek(fd, 0, SEEK_END));
     char ch = 0;
+    int pos = lseek(fd, -1, SEEK_END);
     ssize_t r_value = read(fd, &ch, 1);
     if (r_value == -1) {
         perror("read");
     }
 
-    int pos;
     for (int i = 0; i < 10; i++) {
         ch = 0;
         while (ch != '\n') {
             read(fd, &ch, 1);
+            lseek(fd, pos--, SEEK_SET);
         }
-        pos = fd;
     }
-    printf("exited find start\n");
     return pos + 1;
 }
 
 void tail(int fd) {
-    printf("in tail func\n");
     char buf[BUFSIZE], ch;
     ssize_t r_value, w_value;
-    int cnt = 0, idx = 0, pos = find_start(fd);
+    int idx = 0, pos = find_start(fd);
     lseek(fd, pos, SEEK_SET);
+    printf("pos = %d\n", pos);
 
     do {
         r_value = read(fd, &ch, 1);
@@ -41,6 +47,7 @@ void tail(int fd) {
             break;
         } else if (r_value == 0) {
             ch = '\n';
+            break;
         }
 
         buf[idx++] = ch;
@@ -50,9 +57,6 @@ void tail(int fd) {
                 perror("write");
             }
             idx = 0;
-            if (++cnt == 10) {
-                break;
-            }
         } else if (idx == BUFSIZE) {
             w_value = write(STDOUT_FILENO, buf, idx);
             if (w_value == -1) {
@@ -64,9 +68,11 @@ void tail(int fd) {
 }
 
 void print_header(const char *fName, int i) {
-    char *full = ((i > 1) ? "\n==> " : "==> ");
-    strcat(strcat(full, fName), " <==\n");
-    ssize_t w_value = write(STDOUT_FILENO, full, strlen(full));
+    char header[200];
+    strcat(header, ((i > 1) ? "\n==> " : "==> "));
+    strcat(strcat(header, fName), " <==\n");
+
+    ssize_t w_value = write(STDOUT_FILENO, header, strlen(header));
     if (w_value == -1) {
         perror("write header");
     }
@@ -74,9 +80,10 @@ void print_header(const char *fName, int i) {
 
 int main(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
-        printf("%d\n", i);
         char *fName = argv[i];
-        // print_header(fName, i);
+        if (argc > 2) {
+            print_header(fName, i);
+        }
         int fd = open(fName, O_RDONLY);
         if (fd == -1) {
             perror("open");
