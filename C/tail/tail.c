3777307be3d1da6d927a28c0fd4cc0397432ea8b
Victor <victor@gorchilov.com>
2019-11-28 16:43:59 +0200

    tail

--- C/tail/tail.c
+++ C/tail/tail.c
@@ -5,78 +5,83 @@
 #include <sys/types.h>
 #include <unistd.h>
 #define BUFSIZE 10000
-#define LINES 10
 
-
-void tail(int fd) {
-    char buf[BUFSIZE], ch;
-    ssize_t read_value, write_value;
-    int cnt = 0, idx = 0;
-
-    
-
-    lseek(fd, pos, SEEK_SET);
-    do {
-        read_value = read(fd, &ch, 1);
-        if (read_value == -1) {
-            perror("read");
-            break;
-        } else if (read_value == 0) {  // end of file with no new line
-            ch = '\n';                 // just set a new line. the loop will break due to read_value == 0
-        }
-
-        buf[idx++] = ch;
-        if (ch == '\n') {
-            write(STDOUT_FILENO, buf, idx);
-            idx = 0;
-            if (++cnt == LINES) {
-                break;
-            }
-        } else if (idx == BUFSIZE) {
-            write(STDOUT_FILENO, buf, idx);
-            idx = 0;
-        }
-    }while(42);
-}
-
-int find_start(int fd, int pos) {
-    lseek(fd, pos-1, SEEK_SET);
+int find_start(int fd) {
+    printf("entered find start\n");
+    printf("%lld\n", lseek(fd, 0, SEEK_END));
     char ch = 0;
-    int i;
-    read(fd, &ch, 1);
+    ssize_t r_value = read(fd, &ch, 1);
+    if (r_value == -1) {
+        perror("read");
+    }
+
+    int pos;
     for (int i = 0; i < 10; i++) {
         ch = 0;
         while (ch != '\n') {
             read(fd, &ch, 1);
-            pos = fd - 1;
         }
+        pos = fd;
     }
-    return pos;
+    printf("exited find start\n");
+    return pos + 1;
+}
+
+void tail(int fd) {
+    printf("in tail func\n");
+    char buf[BUFSIZE], ch;
+    ssize_t r_value, w_value;
+    int cnt = 0, idx = 0, pos = find_start(fd);
+    lseek(fd, pos, SEEK_SET);
+
+    do {
+        r_value = read(fd, &ch, 1);
+        if (r_value == -1) {
+            perror("read");
+            break;
+        } else if (r_value == 0) {
+            ch = '\n';
+        }
+
+        buf[idx++] = ch;
+        if (ch == '\n') {
+            w_value = write(STDOUT_FILENO, buf, idx);
+            if (w_value == -1) {
+                perror("write");
+            }
+            idx = 0;
+            if (++cnt == 10) {
+                break;
+            }
+        } else if (idx == BUFSIZE) {
+            w_value = write(STDOUT_FILENO, buf, idx);
+            if (w_value == -1) {
+                perror("write");
+            }
+            idx = 0;
+        }
+    } while (42);
 }
 
 void print_header(const char *fName, int i) {
     char *full = ((i > 1) ? "\n==> " : "==> ");
     strcat(strcat(full, fName), " <==\n");
-    write(STDOUT_FILENO, full, strlen(full));
-}
-
-void test(int fd) {
-    char ch;
-    read(fd, &ch, 1);
-    printf("%c\n", ch);
-    lseek(fd, 20, SEEK_SET);
-    printf("%d\n", fd);
+    ssize_t w_value = write(STDOUT_FILENO, full, strlen(full));
+    if (w_value == -1) {
+        perror("write header");
+    }
 }
 
 int main(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
+        printf("%d\n", i);
         char *fName = argv[i];
+        // print_header(fName, i);
         int fd = open(fName, O_RDONLY);
         if (fd == -1) {
             perror("open");
-            return 1;
         }
-        
+        tail(fd);
     }
-    
+    return 0;
 }
