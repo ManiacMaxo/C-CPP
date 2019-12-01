2a3bd48602f98fcd3d8383e5be36b8642d2ee5bf
Victor <victor@gorchilov.com>
2019-12-01 16:50:12 +0200

    added comments

--- C/tail/tail.c
+++ C/tail/tail.c
@@ -17,115 +17,139 @@ implementation of tail function in c
 
 int multiple_files = 0;
 
-void merror(char *err_msg, char *fName) {
+void error(char *err_msg, char *filename) {
+    /* ------------------------------------------------------------------------
+    FUNCTION: error
+    return error on given message
+    PARAMETERS:
+    error message and filename
+    what kind of error needs to be returned
+    ------------------------------------------------------------------------ */
+
     char buf1[255], buf2[255];
-    sprintf(buf1, err_msg, fName);
+    sprintf(buf1, err_msg, filename);
     sprintf(buf2, "tail: %s", buf1);
     perror(buf2);
 }
 
-void print_header(const char *fName) {
+void print_header(const char *filename) {
+    /* ------------------------------------------------------------------------
+    FUNCTION: print_header
+    print the header of a given file
+    PARAMETERS:
+    filename
+    the name of the file whose header has to be printed
+    ------------------------------------------------------------------------ */
+
     char header[255];
-    sprintf(header, "==> %s <==\n", fName);
+    sprintf(header, "==> %s <==\n", filename);
     if (write(STDOUT_FILENO, header, strlen(header)) == -1) {
-        merror("error writing '%'", "standart ouput");
+        error("error writing '%s'", "standart ouput");
     }
 }
 
-int find_start(int fd, char *fName) {
+int find_start(int fd, char *filename) {
+    /* ------------------------------------------------------------------------
+    FUNCTION: find_start
+    go through the fd backwards until 10 lines are read and return the position
+    PARAMETERS:
+    file descriptor and filename
+    self-explanatory, filename needed for error checking
+    ------------------------------------------------------------------------ */
+
     char ch;
     int i = 0;
     int pos = lseek(fd, -2, SEEK_END);
     if (pos == -1) {
-        merror("error seeking '%s'", fName);
+        error("error seeking '%s'", filename);
         return -1;
     }
     do {
         if (read(fd, &ch, 1) == -1) {
-            merror("error reading '%s'", fName);
+            error("error reading '%s'", filename);
             return -1;
         }
         if (ch == '\n') {
             i++;
         }
         if (lseek(fd, --pos, SEEK_SET) == -1) {  // short file
-            pos--;
-            break;
+            error("error reading '%s'", filename);
+            return -1;
+        }
+        if (pos == 0) {
+            return 0;
         }
     } while (i < 10);
     return pos + 2;
 }
 
-void tail(int fd, char *fName) {
+void tail(int fd, char *filename) {
+    /* ------------------------------------------------------------------------
+    FUNCTION: tail
+    start from position and print unil EOF
+    PARAMETERS:
+    file descriptor and filename
+    self-explanatory, filename needed for error checking
+    ------------------------------------------------------------------------ */
+
     char ch;
-    int pos = find_start(fd, fName);
+    int pos = find_start(fd, filename);
     if (pos == -1) {
         return;
     }
     if (lseek(fd, pos, SEEK_SET) == -1) {
-        merror("error seeking %s", fName);
+        error("error seeking %s", filename);
         return;
     };
 
     if (multiple_files) {
-        print_header(fName);
+        print_header(filename);
     }
     while (read(fd, &ch, 1) > 0) {
         if (write(STDOUT_FILENO, &ch, 1) == -1) {
-            merror("error writing '%'", "standart ouput");
+            error("error writing '%s'", "standart ouput");
         }
     }
     if (ch != '\n') {
         if (write(STDOUT_FILENO, "\n", 1) == -1) {
-            merror("error writing '%'", "standart ouput");
+            error("error writing '%s'", "standart ouput");
         }
     }
+    return;
 }
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
+    if (argc == 1) {  // no file input
         tail(STDIN_FILENO, "standart input");
     } else {
-        if (argc > 2) multiple_files = 1;
-        char *fName;
+        multiple_files = 1;
+        char *filename;
         for (int i = 1; i < argc; i++) {
-            if (argc > 2) {
-                if (i > 1) {
-                    if (write(STDOUT_FILENO, "\n", 1) == -1) {
-                        merror("error writing '%'", "standart ouput");
-                    }
+            if (i > 1) {
+                if (write(STDOUT_FILENO, "\n", 1) == -1) {
+                    error("error writing '%s'", "standart ouput");
                 }
             }
 
             int fd;
             if (argv[i][0] == '-') {
                 fd = STDIN_FILENO;
-                fName = "standart input";
+                filename = "standart input";
             } else {
-                fName = argv[i];
-                // struct stat sb;
-                // if (stat(fName, &sb) == -1) {
-                //     merror("error reading '%s'", fName);
-                //     continue;
-                // }
-                // if (S_ISDIR(sb.st_mode)) {
-                //     merror("error reading '%s'", fName);
-                //     continue;
-                // }
-                fd = open(fName, O_RDONLY);
+                filename = argv[i];
+                fd = open(filename, O_RDONLY);
                 if (fd == -1) {
-                    merror("cannot open '%s' for reading", fName);
+                    error("cannot open '%s' for reading", filename);
                     continue;
                 }
             }
 
-            tail(fd, fName);
+            tail(fd, filename);
             if (fd != STDIN_FILENO) {
                 if (close(fd) == -1) {
-                    merror("error reading '%s'", fName);
+                    error("error reading '%s'", filename);
                 }
             }
         }
     }
-    return 0;
 }
