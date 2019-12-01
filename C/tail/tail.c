a724e5cda97fe1c5fb0ff5ea8603835bf1a1d2f0
Victor <victor@gorchilov.com>
2019-12-01 22:24:03 +0200

    tail

--- C/tail/tail.c
+++ C/tail/tail.c
@@ -11,13 +11,14 @@ implementation of tail function in c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 int multiple_files = 0;
 
-void error(char *err_msg, char *filename) {
+void error(char *err_msg, char *fName) {
     /* ------------------------------------------------------------------------
     FUNCTION: error
     return error on given message
@@ -27,12 +28,12 @@ void error(char *err_msg, char *filename) {
     ------------------------------------------------------------------------ */
 
     char buf1[255], buf2[255];
-    sprintf(buf1, err_msg, filename);
+    sprintf(buf1, err_msg, fName);
     sprintf(buf2, "tail: %s", buf1);
     perror(buf2);
 }
 
-void print_header(const char *filename) {
+void print_header(const char *fName) {
     /* ------------------------------------------------------------------------
     FUNCTION: print_header
     print the header of a given file
@@ -42,13 +43,16 @@ void print_header(const char *filename) {
     ------------------------------------------------------------------------ */
 
     char header[255];
-    sprintf(header, "==> %s <==\n", filename);
-    if (write(STDOUT_FILENO, header, strlen(header)) == -1) {
-        error("error writing '%s'", "standart ouput");
+    if (multiple_files) {
+        sprintf(header, "\n");
+    }
+    sprintf(header, "==> %s <==\n", fName);
+    if (write(STDOUT_FILENO, header, strlen(header)) == -1 || errno == 28) {
+        error("error writing '%s'", "standard ouput");
     }
 }
 
-int find_start(int fd, char *filename) {
+int find_start(int fd, char *fName) {
     /* ------------------------------------------------------------------------
     FUNCTION: find_start
     go through the fd backwards until 10 lines are read and return the position
@@ -61,29 +65,26 @@ int find_start(int fd, char *filename) {
     int i = 0;
     int pos = lseek(fd, -2, SEEK_END);
     if (pos == -1) {
-        error("error seeking '%s'", filename);
+        error("error seeking '%s'", fName);
         return -1;
     }
     do {
         if (read(fd, &ch, 1) == -1) {
-            error("error reading '%s'", filename);
+            error("error reading '%s'", fName);
             return -1;
         }
         if (ch == '\n') {
             i++;
         }
         if (lseek(fd, --pos, SEEK_SET) == -1) {  // short file
-            error("error reading '%s'", filename);
-            return -1;
-        }
-        if (pos == 0) {
-            return 0;
+            pos--;
+            break;
         }
     } while (i < 10);
     return pos + 2;
 }
 
-void tail(int fd, char *filename) {
+void tail(int fd, char *fName) {
     /* ------------------------------------------------------------------------
     FUNCTION: tail
     start from position and print unil EOF
@@ -93,63 +94,77 @@ void tail(int fd, char *filename) {
     ------------------------------------------------------------------------ */
 
     char ch;
-    int pos = find_start(fd, filename);
-    if (pos == -1) {
+    int pos = find_start(fd, fName);
+    if (pos == -1) {  // error with file
         return;
     }
     if (lseek(fd, pos, SEEK_SET) == -1) {
-        error("error seeking %s", filename);
+        error("error seeking %s", fName);
         return;
-    };
+    }
 
-    if (multiple_files) {
-        print_header(filename);
+    if (multiple_files && fd != STDIN_FILENO) {
+        print_header(fName);
     }
     while (read(fd, &ch, 1) > 0) {
-        if (write(STDOUT_FILENO, &ch, 1) == -1) {
-            error("error writing '%s'", "standart ouput");
+        if (write(STDOUT_FILENO, &ch, 1) == -1 || errno == 28) {
+            error("error writing '%s'", "standard ouput");
         }
     }
     if (ch != '\n') {
-        if (write(STDOUT_FILENO, "\n", 1) == -1) {
-            error("error writing '%s'", "standart ouput");
+        if (write(STDOUT_FILENO, "\n", 1) == -1 || errno == 28) {
+            error("error writing '%s'", "standard ouput");
         }
     }
-    return;
+}
+
+void read_std() {
+    char ch;
+    int fd = open("temp.txt", O_RDWR | O_CREAT, 0644);
+    if (fd == -1) {
+        error("cannot open '%s'", "temp.txt");
+    }
+
+    while (read(STDIN_FILENO, &ch, 1)) {
+        write(fd, &ch, 1);
+    }
+
+    tail(fd, "standard input");
+    if (close(fd) == -1) {
+        error("error reading '%s'", "temp.txt");
+    }
 }
 
 int main(int argc, char **argv) {
-    if (argc == 1) {  // no file input
-        tail(STDIN_FILENO, "standart input");
+    if (argc == 1) {  // no file arguments
+        read_std();
     } else {
-        multiple_files = 1;
-        char *filename;
-        for (int i = 1; i < argc; i++) {
-            if (i > 1) {
-                if (write(STDOUT_FILENO, "\n", 1) == -1) {
-                    error("error writing '%s'", "standart ouput");
-                }
-            }
+        if (argc > 2) multiple_files = 1;
+        char *fName;
 
+        for (int i = 1; i < argc; i++) {  // cycle through all given files
             int fd;
             if (argv[i][0] == '-') {
-                fd = STDIN_FILENO;
-                filename = "standart input";
-            } else {
-                filename = argv[i];
-                fd = open(filename, O_RDONLY);
+                read_std();
+                continue;
+            } else {  // normal file
+                fName = argv[i];
+                fd = open(fName, O_RDONLY);
                 if (fd == -1) {
-                    error("cannot open '%s' for reading", filename);
+                    error("cannot open '%s' for reading", fName);
                     continue;
                 }
             }
-
-            tail(fd, filename);
-            if (fd != STDIN_FILENO) {
-                if (close(fd) == -1) {
-                    error("error reading '%s'", filename);
+            if (multiple_files && i > 1) {  // apply headers only if files are > 1
+                if (write(STDOUT_FILENO, "\n", 1) == -1 || errno == 28) {
+                    error("error writing '%s'", "standard ouput");
                 }
+            }
+            tail(fd, fName);
+            if (close(fd) == -1) {
+                error("error reading '%s'", fName);
             }
         }
     }
+    return 0;
 }
