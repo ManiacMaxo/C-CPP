0bd96f906b33588710670a3adf4a3b923b137be2
Victor <victor@gorchilov.com>
2019-12-05 11:22:27 +0200

    watch

--- C/watch/watch.c
+++ C/watch/watch.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int compile_func(char *func) {
-    pid_t pid = fork();
+void compile_func(char **func) {
+    pid_t pid;
+    int status;
+
+    if ((pid = fork()) < 0) {
+        perror("execution failed");
+        exit(1);
+    } else if (pid == 0) {
+        if (execvp(*func, func) < 0) {
+            perror("execution failed");
+            exit(1);
+        }
+    } else {
+        while (wait(&status) != pid) {
+        }
+    }
 }
 
 int main(int argc, char **argv) {
-    char *func;
-
+    char *func = "ls";
+    execvp(func, "-la");
+    // char *func = argv[1];
+    int i = 0;
     do {
-        printf("Hey!\n");
+        printf("i = %d\n", i++);
+        // compile_func(func);
         sleep(2);
     } while (42);
 }
