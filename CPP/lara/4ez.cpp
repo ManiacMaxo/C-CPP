ae6cb7cae9dc41d2d3afbeaf736a6c6fa87684c3
Victor <victor@gorchilov.com>
2019-12-06 08:32:12 +0200

    reformatting

--- CPP/lara/4ez.cpp
+++ CPP/lara/4ez.cpp
@@ -10,7 +10,8 @@ class clientPersonOne {
     float usedPowerDay;
     float usedPowerNight;
     float calcBill() {
-        return (delivery + distribution + tariffDay) * usedPowerDay + (delivery + distribution + tariffNight) * usedPowerNight;
+        return (delivery + distribution + tariffDay) * usedPowerDay +
+               (delivery + distribution + tariffNight) * usedPowerNight;
     }
 };
 
@@ -20,27 +21,20 @@ class clientPersonTwo {
     float distribution;
     float tariff;
     float usedPower;
-    float calcBill() {
-        return (delivery + distribution + tariff) * usedPower;
-    }
+    float calcBill() { return (delivery + distribution + tariff) * usedPower; }
 };
 
 class clientInstitutionOne : clientPersonOne {
     float highVoltage;
     float calcBill() {
-        return (delivery + distribution + tariffDay + highVoltage) * usedPowerDay + (delivery + distribution + tariffNight + highVoltage) * usedPowerNight;
+        return (delivery + distribution + tariffDay + highVoltage) * usedPowerDay +
+               (delivery + distribution + tariffNight + highVoltage) * usedPowerNight;
     }
 };
 
 class clientInstitutionTwo : clientPersonTwo {
     float highVoltage;
-    float calcBill() {
-        return (delivery + distribution + tariff + highVoltage) * usedPower;
-    }
+    float calcBill() { return (delivery + distribution + tariff + highVoltage) * usedPower; }
 };
 
-int main(){
-    
-
-
-}
+int main() {}
