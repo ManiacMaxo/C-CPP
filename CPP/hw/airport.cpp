f0a91dc3a55b6a22e4faafc9950dad9008e698d5
Victor <victor@gorchilov.com>
2019-12-10 09:57:43 +0200

    inheritance exercise

--- CPP/hw/airport.cpp
+++ CPP/hw/airport.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Airplane {
+   protected:
     int size;
     char flag;
     float speed;
     float fuel_capacity;
     float fuel_economy;
     float X;
-    // оператор << който извежда всичката информация за самолета
+
    public:
     Airplane operator<<(Airplane plane) {
-        cout << "size: " << size << endl;
-        cout << "flag: " << flag << endl;
-        cout << "speed: " << speed << endl;
+        cout << "size:\t" << size << endl;
+        cout << "flag:\t" << flag << endl;
+        cout << "speed:\t" << speed << endl;
+        cout << "fuel capacity:\t" << fuel_capacity << endl;
+        cout << "fuel economy:\t" << fuel_economy << endl;
+        cout << "position:\t" << X << endl;
     }
+};
+
+class Passenger : Airplane {
+    int seats;
+    int passengers;
+    int exits;
+};
+
+class Military : Airplane {
+    float power;
+};
+
+class Cargo : Airplane {
+    float cargo_capacity;
+    float weight;
+};
+
+class Airport {
+    int num_planes;
+    int max_size;
+    char flags;
+    float X;
+    vector<Passenger> passenger_planes;
+    vector<Military> military_planes;
+    vector<Cargo> cargo_planes;
 };
 
 int main() {}
