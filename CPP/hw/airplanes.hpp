64df9bf9fddb4e97a85378e65900856462b398d8
Victor <victor@gorchilov.com>
2019-12-11 18:21:35 +0200

    exercie for test

---
+++ CPP/hw/airplanes.hpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class Airplane {
+    unsigned int size;
+    unsigned int flight_num;
+    bool military;
+    std::string model;
+    double speed;
+    double fuel;
+    double fuel_efficiency;
+    double x;
+};
