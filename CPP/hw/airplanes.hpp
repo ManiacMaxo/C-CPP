9472b66dcc6ef03fb5b124370a08e9fde7d6b1ab
Victor <victor@gorchilov.com>
2019-12-11 22:18:17 +0200

    airport

--- CPP/hw/airplanes.hpp
+++ CPP/hw/airplanes.hpp
@@ -1,7 +1,7 @@
 #include <string>
 
 class Airplane {
-   public:
+   protected:
     unsigned int size;
     unsigned int flight_num;
     bool military;
@@ -10,22 +10,29 @@ class Airplane {
     double fuel;
     double fuel_efficiency;
     double x;
+
+   public:
+    unsigned int get_size() { return size; }
+    unsigned int get_flight_num() { return flight_num; }
+    bool get_military() { return military; }
+    std::string get_model() { return model; }
+    double get_speed() { return speed; }
+    double get_fuel() { return fuel; }
+    double get_fuel_efficiency() { return fuel_efficiency; }
+    double get_x() { return x; }
 };
 
 class PassengerPlane : Airplane {
-   public:
     int passengers;
     int seats;
     int exits;
 };
 
 class CargoPlane : Airplane {
-   public:
     double weight;
     double max_weight;
 };
 
 class MilitaryPlane {
-   public:
     double power;
 };
--- CPP/hw/airport.hpp
+++ CPP/hw/airport.hpp
@@ -10,18 +10,20 @@ class Airport {
     double x;
 
    public:
-    bool can_fit_plane(Airplane const& plane) {
-        if (max_planes < planes.size && plane.size < max_plane_size) {
-            if ((plane.military && accepts_military) ||
-                (!plane.military && accepts_passenger)) {
+    class LandPlaneError {};
+    bool can_fit_plane(Airplane& plane) {
+        if (max_planes < planes.size && plane.get_size() < max_plane_size) {
+            if ((plane.get_military() && accepts_military) ||
+                (!plane.get_military() && accepts_passenger)) {
                 return 1;
             }
         }
         return 0;
     }
     void land_plane(Airplane const& plane) {
-        if (can_fit_plane) {
-            planes.push_back(plane);
+        if (!can_fit_plane) {
+            throw LandPlaneError();
         }
+        planes.push_back(plane);
     }
 };
