285f8784c25fd1d0797927cc1009a15505595ae8
Victor <victor@gorchilov.com>
2020-02-23 11:25:06 +0200

    [Update] threads

---
+++ C/threads.c
@@ -0,0 +1,17 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *work(void *arg) {
+    int id = (int)arg;
+    free(arg);
+    printf("Hello from thread %d\n", id);
+}
+
+int main() {
+    pthread_t threads[10];
+    for (int i = 0; i < 10; i++) {
+        int *p = malloc(sizeof(int));
+        pthread_create(&threads[i], NULL, work, p);
+    }
+}
