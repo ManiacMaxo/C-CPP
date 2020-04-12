6cd3582cf19c49aec3795eab5156b852f3ca80ed
Victor <victor@gorchilov.com>
2020-04-12 22:25:02 +0300

    [Update] for me it works, for them not so much

--- C/starcraft/main.c
+++ C/starcraft/main.c
@@ -6,7 +6,6 @@
  * FILE PURPOSE:
  * very simple command line starcraft with a few interactions
  ***************************************************************************/
-// #include <curses.h> // for getch - non blocking input
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,15 +21,17 @@ typedef struct {
     int num_workers;
     pthread_t *workers;
     int soldiers;
+    pthread_mutex_t mutex;
 } CommandCenter;
 
 // initialize functions
 void *worker(void *arg);
-void addUnit(char unit);
+void *addUnit(void *arg);
 
 // global variables
 int total_m_blocks = 2;
 int num_m_blocks = 2;
+pthread_mutex_t num_m_blocks_mutex = PTHREAD_MUTEX_INITIALIZER;
 MineralBlock *m_blocks;
 CommandCenter center;
 
@@ -44,40 +45,44 @@ int main(int argc, char **argv) {
 
     m_blocks = malloc(sizeof(MineralBlock) * total_m_blocks);
     for (int i = 0; i < total_m_blocks; i++) {
-        m_blocks[i].mass = 50;
-        pthread_mutex_init(&m_blocks[i].mutex, NULL);
+        m_blocks[i].mass = 500;
+        if (pthread_mutex_init(&m_blocks[i].mutex, NULL)) {
+            perror("pthread_mutex_init");
+        }
     }
 
+    // init input thread
+    pthread_t addUnitThread;
+    if (pthread_create(&addUnitThread, NULL, addUnit, NULL)) {
+        perror("pthread_create");
+    }
+
+    // init Command Center
+    pthread_mutex_init(&center.mutex, NULL);
     center.minerals = 0;
-    center.soldiers = 20;
-    center.workers = malloc(sizeof(pthread_t) * center.num_workers);
+    center.soldiers = 0;
     center.num_workers = 5;
+    center.workers = malloc(sizeof(pthread_t) * center.num_workers);
     for (int i = 0; i < center.num_workers; i++) {
-        pthread_create(&center.workers[i], NULL, worker, (void *)i);
-        // pthread_detach(center.workers[i]);
-        /* creates a thread that doesn't need to be joined
-        by another thread and doesn't block the function */
-        sleep(1);
+        if (pthread_create(&center.workers[i], NULL, worker, (void *)i)) {
+            perror("pthread_create");
+        }
+        pthread_detach(center.workers[i]);
     }
 
     // --------------- runtime ---------------
 
-    while (42) {
-        if ((center.minerals < 50 || center.soldiers >= 20) && num_m_blocks <= 0) {
-            break;
-        }
-
-        char temp = '0';
-        temp = getchar();
-        if (temp == 's' || temp == 'm') {
-            addUnit(temp);
-        }
+    while (center.soldiers < 20) {
+        usleep(1000);
     }
+    sleep(5);
+    pthread_cancel(addUnitThread);
 
     // --------------- finish ---------------
 
     printf("Map minerals %d, player minerals %d, SCVs %d, Marines %d\n",
-           total_m_blocks, center.minerals, center.num_workers, center.soldiers);
+           total_m_blocks * 500, center.minerals, center.num_workers,
+           center.soldiers);
     m_blocks = malloc(sizeof(MineralBlock) * total_m_blocks);
 
     free(m_blocks);
@@ -94,73 +99,90 @@ void *worker(void *arg) {
      * arg : int (void *), id of worker
      **************************************************************************/
     int num = (int)arg;
-    while (42) {
-        if (num_m_blocks <= 0) {
-            pthread_exit(NULL);
-        }
+    while (num_m_blocks > 0) {
         for (int i = 0; i < total_m_blocks; i++) {
-            if (m_blocks[i].mass == 0) {  // is block mined
+            if (m_blocks[i].mass <= 0) {  // is block mined
                 continue;
             }
-            sleep(3);  // move to block
 
-            if (pthread_mutex_trylock(&m_blocks[i].mutex) != 0) {
+            sleep(3);  // move to block
+            if (pthread_mutex_trylock(&m_blocks[i].mutex) != 0 ||
+                m_blocks[i].mass <= 0) {
                 continue;
             }
-            printf("SCV %d is mining from mineral block %d\n", num, i);
+            printf("SCV %d is mining from mineral block %d\n", num + 1, i + 1);
             int val = m_blocks[i].mass < 8 ? m_blocks[i].mass : 8;
-            // remove 8 from block, if block is smaller then take all
-            m_blocks[i].mass -= 8;
-            if (m_blocks[i].mass <= 0) {
+            // remove 8 from block; if block is less remove remaining
+            m_blocks[i].mass -= val;
+
+            pthread_mutex_lock(&num_m_blocks_mutex);
+            if (m_blocks[i].mass == 0) {
                 num_m_blocks--;
-                printf("number of blocks: %d\n", num_m_blocks);
+                // printf("number of blocks: %d\n", num_m_blocks);
             }
+            pthread_mutex_unlock(&num_m_blocks_mutex);
             pthread_mutex_unlock(&m_blocks[i].mutex);
-            printf("SCV %d is transporting minerals\n", num);
+            printf("SCV %d is transporting minerals\n", num + 1);
             sleep(2);
+            while (pthread_mutex_trylock(&center.mutex) != 0) {
+                usleep(1000);
+            }
             center.minerals += val;
-            printf("SCV %d delivered minerals to the Command center\n", num);
+            pthread_mutex_unlock(&center.mutex);
+            printf("SCV %d delivered minerals to the Command center\n", num + 1);
             break;
         }
     }
     pthread_exit(NULL);
 }
 
-void addUnit(char unit) {
+void *addUnit(void *arg) {
     /**************************************************************************
      * FUNCTION: addUnit
      * function for creating a soldier or worker and starting worker
      * PARAMETERS:
-     * arg : int (void *), id of worker
+     * none
      **************************************************************************/
-    printf("Total minerals: %d\n", center.minerals);
-    if (center.soldiers + center.num_workers == 200 || center.minerals < 50) {
-        printf("\033[0;31m");
-        printf("Not enought minerals or too many units!\n");
-        printf("\033[0m");
-        return;
-    }
-    switch (unit) {
-        case 'm':  // soldier
-            center.soldiers++;
-            center.minerals -= 50;
-            sleep(1);
-            printf("\033[0;33m");
-            printf("You wanna piece of me boy?\n");
+    do {
+        char unit;
+        do {
+            unit = getchar();
+        } while (unit != 'm' && unit != 's' && unit != 't');
+
+        if (center.soldiers + center.num_workers == 200 || center.minerals < 50) {
+            printf("\033[0;31m");
+            printf("Not enough minerals.\n");
             printf("\033[0m");
-            break;
-        case 's':  // worker
-            center.num_workers++;
-            center.minerals -= 50;
-            center.workers =
-                realloc(center.workers, sizeof(pthread_t) * center.num_workers);
-            pthread_create(&center.workers[center.num_workers - 1], NULL, worker,
-                           (void *)center.num_workers - 1);
-            sleep(4);
-            printf("\033[0;32m");
-            printf("SCV is good to go\n");
-            printf("\033[0m");
-            pthread_detach(center.workers[center.num_workers]);
-            break;
-    }
+            continue;
+        }
+
+        switch (unit) {
+            case 'm':  // soldier
+                center.soldiers++;
+                center.minerals -= 50;
+                sleep(1);
+                printf("You wanna piece of me, boy?\n");
+                break;
+            case 's':  // worker
+                center.num_workers++;
+                center.minerals -= 50;
+                center.workers =
+                    realloc(center.workers, sizeof(pthread_t) * center.num_workers);
+                sleep(4);
+                if (pthread_create(&center.workers[center.num_workers - 1], NULL,
+                                   worker, (void *)center.num_workers - 1)) {
+                    perror("pthread_create");
+                }
+                pthread_detach(center.workers[center.num_workers - 1]);
+                printf("SCV is good to go, sir.\n");
+                break;
+                // case 't':
+                //     printf("Total minerals: %d\n", center.minerals);
+                //     for (int i = 0; i < total_m_blocks; i++) {
+                //         printf("block %d mass: %d\n", i, m_blocks[i].mass);
+                //     }
+                //     break;
+        }
+    } while (42);
+    pthread_exit(NULL);
 }
